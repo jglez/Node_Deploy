0b2d854bd8e5ccbd4935713ff9edd2559686361c
jglez <jglez.cs@gmail.com>
2021-03-11 12:31:42 -0600

    Created catch-all endpoint and build for React app

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 require('dotenv').config()
 
+// Import Path from Node.js
+const path = require('path')
+
 console.log('it works!!!')
 
 const express = require('express')
@@ -9,6 +12,9 @@ const server = express()
 
 // parse JSON bodies
 server.use(express.json())
+
+// Tell Express where our static assets are regardless of machine
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 // console.log(process.env.NODE_ENV)
 
@@ -23,8 +29,14 @@ if (process.env.NODE_ENV === 'development') {
   server.use(cors())
 }
 
-server.use('*', (req, res) => {
-  res.send(`<h1>GREAT SCOTT, IT'S WORKING!!!</h1>`)
+// API comes earlier in the pipeline
+server.get('/api/hello', (req, res) => {
+  res.json({ message: 'hello' })
+})
+
+// Catch all endpoint that just sends back index.html
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
 })
 
 // Fallback port
@@ -33,3 +45,5 @@ const PORT = process.env.PORT || 4000
 server.listen(PORT, () => {
   console.log(`listening on ${PORT}`)
 })
+
+// Serve React app using express
